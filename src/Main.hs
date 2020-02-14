0d2c0c853292da2fe8236d4f3de2578aedb1292d
G. Allais <guillaume.allais@ens-lyon.org>
2020-02-14 23:41:47 +0000

    Setting up 'github actions' (#1)
    There are no tests running yet. But it will make sense to have
    some fairly early on. Agda uses tasty with [great success](https://github.com/agda/agda/blob/master/test/Succeed/Tests.hs).
    
    
    ---------------------------------------------------------------
    * [ init ] cabal file
    
    * Create haskell.yml
    
    * [ fix ] haskell.yaml
    
    Cf. https://github.com/actions/starter-workflows/issues/228
    
    * [ fix ] cabal file mentioning imaginary files
    
    * [ cabal ] add Main.hs

---
+++ src/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import Data.Bwd
+import Shonkier.Syntax
+import Shonkier.Semantics
+import Shonkier.Examples
+import Shonkier.Parser
+import Shonkier.Parser.Examples
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
