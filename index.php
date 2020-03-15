4ea99dae4b5dc139d09c6337c435c6b95f09c44c
Conor McBride <conor@strictlypositive.org>
2020-03-15 13:54:18 +0000

    index.php wrapper for web version; this won't do anything sensible yet

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+$user_id = escapeshellarg($_SERVER['HTTP_CIS_REMOTE_USER']);
+$page_id = escapeshellarg($_GET["page"]);
+
+$descriptorspec = array(
+   0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+   1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+   2 => array("file", "/tmp/error-output.txt", "a") // stderr is a file to write to
+);
+
+$cmd = "mary -web $user_id $page_id";
+
+$cwd = NULL;
+$env = NULL;
+
+$process = proc_open($cmd, $descriptorspec, $pipes, $cwd, $env);
+
+if (is_resource($process)) {
+    // $pipes now looks like this:
+    // 0 => writeable handle connected to child stdin
+    // 1 => readable handle connected to child stdout
+    // Any error output will be appended to /tmp/error-output.txt
+
+    fwrite($pipes[0], serialize($_POST));
+    fwrite($pipes[0], serialize($_GET));
+    fclose($pipes[0]);
+
+    echo stream_get_contents($pipes[1]);
+    fclose($pipes[1]);
+
+    // It is important that you close any pipes before calling
+    // proc_close in order to avoid a deadlock
+    $return_value = proc_close($process);
+};
+
+?>
