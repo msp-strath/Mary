d3f00a1df1fd73c0c2cb33d74813c067910c9b22
Fredrik Nordvall Forsberg <fredrik.nordvall-forsberg@strath.ac.uk>
2020-03-16 19:18:12 +0000

    [ refactor ] use optparse-applicative

--- mary.hs
+++ mary.hs
@@ -1,10 +1,13 @@
 module Main where
 
+import Data.Semigroup ((<>))
 import Data.Text.IO as TIO
 
-import System.Environment
+import Text.Pandoc.JSON (toJSONFilter)
 
-import Text.Pandoc.JSON
+import Options.Applicative
+
+import System.Environment
 
 import Shonkier
 
@@ -13,16 +16,55 @@ import Mary.ServePage
 import Mary.ServeWeb
 
 main :: IO ()
-main = do
-  xs <- getArgs
-  case xs of
-    ["-pandoc"]               -> toJSONFilter process
-    ["-shonkier", filename]   -> interpretShonkier filename
-    ["-shonkierjs", filename] -> compileShonkier filename >>= TIO.putStrLn
-    ["-page", filename]       -> servePage testConfig filename >>= TIO.putStrLn
-    ["-web", pandoc, sitesRoot, user, page] -> do
-      mary    <- getExecutablePath
-      content <- serveWeb Config{..} sitesRoot user page
-      TIO.putStrLn content
-    _ -> TIO.putStr "# mary says\nI don't know what you're on about.\n\n"
+main = customExecParser (prefs showHelpOnEmpty) opts >>= \case
+  Pandoc              -> toJSONFilter process
+  Shonkier filename   -> interpretShonkier filename
+  Shonkierjs filename -> compileShonkier filename >>= TIO.putStrLn
+  Page filename       -> servePage testConfig filename >>= TIO.putStrLn
+  Web{..}             -> do
+    mary    <- getExecutablePath
+    content <- serveWeb Config{..} sitesRoot user page
+    TIO.putStrLn content
+  where
+    opts = info (optsParser <**> helper)
+      ( fullDesc
+     <> header "Mary - a content delivery and assessment engine")
 
+data Options
+  = Pandoc
+  | Shonkier   { filename :: String }
+  | Shonkierjs { filename :: String }
+  | Page       { filename :: String }
+  | Web        { pandoc    :: String
+               , sitesRoot :: String
+               , user      :: String
+               , page      :: String
+               }
+
+optsParser :: Parser Options
+optsParser = subparser
+  ( command' "pandoc"
+             (pure Pandoc)
+             "Act as a Pandoc filter"
+ <> command' "shonkier"
+             (Shonkier <$> strArgument
+                (metavar "FILE" <> action "file" <> help "Input Shonkier program."))
+             "Interpret shonkier program"
+ <> command' "shonkierjs"
+             (Shonkierjs <$> strArgument
+                  (metavar "FILE" <> action "file" <> help "Source Shonkier program."))
+             "Compile shonkier program to javascript"
+ <> command' "page"
+             (Page <$> strArgument
+                  (metavar "FILE" <> action "file" <> help "Input Mary file"))
+             "Generate HTML from Mary file"
+ <> command' "web"
+             (Web <$> strArgument (metavar "PANDOC" <> action "file" <> help "Path to pandoc.")
+                  <*> strArgument (metavar "ROOT" <> action "directory" <> help "Path to site root.")
+                  <*> strArgument (metavar "USER" <> action "user" <> help "Username.")
+                  <*> strArgument (metavar "PAGE" <> action "file" <> help "Page to serve."))
+             "Serve webpage")
+  where
+    command' :: String -> Parser a -> String -> Mod CommandFields a
+    command' label parser description =
+      command label (info parser (progDesc description))
