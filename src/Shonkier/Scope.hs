e46fb2ab9c36861181677422731648b1fe82d4e4
Guillaume Allais <guillaume.allais@ens-lyon.org>
2020-03-18 10:45:50 +0000

    [ compat ] import (<>)

--- src/Shonkier/Scope.hs
+++ src/Shonkier/Scope.hs
@@ -18,6 +18,7 @@ import qualified Data.Map as Map
 import Data.Maybe (fromMaybe)
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Data.Semigroup ((<>)) -- needed for ghc versions <= 8.2.2
 
 import Shonkier.Syntax
 
