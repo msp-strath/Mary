7f79d2f8a6d9adc08e7369ee5df05428cfa0530c
Fredrik Nordvall Forsberg <fredrik.nordvall-forsberg@strath.ac.uk>
2020-04-07 15:07:21 +0100

    Make servepage behave more like index.php; make mary tests use servepage

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 SOURCE = $(shell find src -name '*.lhs' -o -name '*.hs')
 
-.PHONY: clean install-hasktags TAGS
+.PHONY: clean install-hasktags TAGS bash-completion
 
 # From https://stackoverflow.com/questions/18649427/using-a-makefile-with-cabal
 # If 'cabal install' fails in building or installing, the
@@ -22,10 +22,23 @@ install-hasktags:
 	cabal update
 	cabal install hasktags
 
-.PHONY: test
+.PHONY: test test-mary test-js test-shonkier
 test:
 	cabal new-run mary-tests -- -i
 #	runhaskell -itest test/Test/Main.hs -i
 
+test-mary:
+	cabal new-run mary-tests -- -i -p Mary
+
+test-shonkier:
+	cabal new-run mary-tests -- -i -p '$$2 == "Shonkier"'
+
+test-js:
+	cabal new-run mary-tests -- -i -p ShonkierJS
+
 TAGS:
 	hasktags --etags .
+
+bash-completion:
+# Use as follows: source <(make bash-completion)
+	mary --bash-completion-script `which mary`
--- src/Mary/ServePage.hs
+++ src/Mary/ServePage.hs
@@ -2,33 +2,44 @@
 
 module Mary.ServePage where
 
+import Control.Arrow
+
+import Data.ByteString.Lazy as B
 import Data.Text
+import Data.Text.Encoding (encodeUtf8)
 import Data.Text.IO as TIO
 
+import Data.PHPSession
+
 import System.Process
+import System.IO
 
 data Config = Config
   { mary   :: FilePath
   , pandoc :: FilePath
+  , user   :: String
+  , siteRoot :: String
   }
 
-testConfig :: Config
-testConfig = Config
-  { mary   = "mary"
-  , pandoc = "pandoc"
-  }
-
-servePage :: Config -> FilePath -> IO Text
-servePage Config{..} inp =
-  withCreateProcess ((proc pandoc ["-s", inp, "-f", "markdown", "-t", "json"])
-                     { std_out = CreatePipe
+servePage :: Config
+          -> [(Text, Text)]  -- POST data
+          -> [(Text, Text)]  -- GET  data
+          -> FilePath        -- input file
+          -> IO Text
+servePage Config{..} post get file =
+  withCreateProcess ((proc mary ["find", "--user", user, siteRoot, file])
+                     { std_in  = CreatePipe
+                     , std_out = CreatePipe
+                     }) $ \ (Just hin) (Just hmaryfind) _ _ -> do
+    B.hPut hin $ phpify post
+    B.hPut hin $ phpify get
+    hClose hin
+    withCreateProcess ((proc pandoc ["--data-dir=data", "--standalone", "-f", "markdown", "--filter=marypandoc", "-t", "html", "--template", "templates/mary.html5"])
+                     { std_in  = UseHandle hmaryfind
+                     , std_out = CreatePipe
                      }) $ \ _ (Just hpandoc) _ _ ->
-  withCreateProcess ((proc mary ["pandoc"])
-                     { std_in  = UseHandle hpandoc
-                     , std_out = CreatePipe
-                     }) $ \ _ (Just hmary) _ _ ->
-  withCreateProcess ((proc pandoc ["-s", "-f", "json", "-t", "html", "--template", "templates/mary.html5"])
-                     { std_in = UseHandle hmary
-                     , std_out = CreatePipe
-                     }) $ \ _ (Just hout) _ _ ->
-  TIO.hGetContents hout
+      TIO.hGetContents hpandoc
+  where
+    encString = PHPSessionValueString . B.fromStrict . encodeUtf8
+    phpify a = encodePHPSessionValue $ PHPSessionValueArray $
+                 fmap (encString *** encString) a
